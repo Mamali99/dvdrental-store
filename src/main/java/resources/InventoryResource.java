c2b8fe6b5970fa2d57cd32e6005b7b097e2d4efc
Mohammad Ghalandari <m.ghalandari@ostfalia.de>
2023-10-10 17:51:59 +0200

    Das Projekt ist mit Wildfly verbunden und funktioniert

--- src/main/java/resources/InventoryResource.java
+++ src/main/java/resources/InventoryResource.java
@@ -1,20 +1,26 @@
 package resources;
 
+import entities.Inventory;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import services.InventoryService;
 
 @Path("/inventories")
 public class InventoryResource {
+
+    @Inject
+    InventoryService inventoryService;
 
     @GET
     @Path("/film/{filmId}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getInventoriesByFilmId(@PathParam("filmId") int filmId) {
-        // Implementierung der Logik
+
         return Response.ok().build();
     }
 
@@ -22,7 +28,7 @@ public class InventoryResource {
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getInventoryById(@PathParam("id") int id) {
-        // Implementierung der Logik
-        return Response.ok().build();
+        Inventory i = inventoryService.getInventoryById(id);
+        return Response.ok(i).build();
     }
 }
