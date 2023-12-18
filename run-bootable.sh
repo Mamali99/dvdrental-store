afca50af6e8587c01d3a39958511619fd924ab79
Mohammad Ghalandari <m.ghalandari@ostfalia.de>
2023-12-18 15:53:03 +0100

    mvn clean package wurde in run-bootalbe.sh eingefügt.

--- run-bootable.sh
+++ run-bootable.sh
@@ -2,7 +2,7 @@
 
 # Setze Umgebungsvariablen für die Datenbankverbindung
 export POSTGRES_HOST=localhost
-export POSTGRES_PORT=54322 # muss alle 5432 sein
+export POSTGRES_PORT=54322
 export POSTGRES_DB=dvdrentalstore
 export POSTGRES_USER=postgres
 export POSTGRES_PASSWORD=trust
@@ -10,14 +10,23 @@ export POSTGRES_PASSWORD=trust
 # Setze den Port, auf dem der Service laufen soll
 export HTTP_PORT=8082
 
-# Führe den bootfähigen JAR aus mit den Systemeigenschaften
-java -Djboss.http.port=${HTTP_PORT} \
-     -Djboss.bind.address=0.0.0.0 \
-     -Djboss.bind.address.management=0.0.0.0 \
-     -Djboss.management.http.port=9992 \
-     -Denv.POSTGRES_HOST=${POSTGRES_HOST} \
-     -Denv.POSTGRES_PORT=${POSTGRES_PORT} \
-     -Denv.POSTGRES_DB=${POSTGRES_DB} \
-     -Denv.POSTGRES_USER=${POSTGRES_USER} \
-     -Denv.POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
-     -jar target/dvdrental-store-bootable.jar
+# Führe Maven Clean und Package aus
+mvn clean package
+
+# Überprüfe, ob der Build erfolgreich war
+if [ -f "target/dvdrental-store-bootable.jar" ]; then
+    # Führe den bootfähigen JAR aus mit den Systemeigenschaften
+    java -Djboss.http.port=${HTTP_PORT} \
+         -Djboss.bind.address=0.0.0.0 \
+         -Djboss.bind.address.management=0.0.0.0 \
+         -Djboss.management.http.port=9992 \
+         -Denv.POSTGRES_HOST=${POSTGRES_HOST} \
+         -Denv.POSTGRES_PORT=${POSTGRES_PORT} \
+         -Denv.POSTGRES_DB=${POSTGRES_DB} \
+         -Denv.POSTGRES_USER=${POSTGRES_USER} \
+         -Denv.POSTGRES_PASSWORD=${POSTGRES_PASSWORD} \
+         -jar target/dvdrental-store-bootable.jar
+else
+    echo "Fehler beim Erstellen der JAR-Datei"
+    exit 1
+fi
